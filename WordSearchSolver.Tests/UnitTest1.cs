4511652ca410b704e0de6a1c12e0ee138c4b2e2e
Dominic Hemken <dphemken@gmail.com>
2023-06-14 09:21:27 -0500

    Add project files.

---
+++ WordSearchSolver.Tests/UnitTest1.cs
@@ -0,0 +1,65 @@
+namespace WordSearchSolver.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SolverShouldFindWords()
+        {
+            var sut = new Solver();
+            sut.Board = GetTestBoard();
+            sut.AllowSingleLetterWords = true;
+            var results = sut.FindWords();
+            var expectedWords = GetTestWords();
+            Assert.GreaterOrEqual(results.Count,expectedWords.Count);
+            var unfoundWords =
+                expectedWords.Where(word => results.All(result => result.Word.ToUpper() != word.ToUpper()))
+                    .ToList();
+            Assert.False(unfoundWords.Any());
+        }
+
+        private char[][] GetTestBoard()
+        {
+            var b = new List<char[]>();
+            b.Add(new[] { 'l', 'e', 'c', 'w', 'o', 't', 'a', 'h', 'e', 'a', 'd', 'c', 'e', 't' });
+            b.Add(new[] { 't', 'a', 'l', 'w', 's', 'r', 'a', 'e', 'k', 'l', 'y', 'i', 'a', 'b' });
+            b.Add(new[] { 'o', 'r', 'o', 'i', 't', 'e', 'e', 't', 'h', 'k', 'y', 'o', 'a', 'w' });
+            b.Add(new[] { 'g', 'o', 'd', 'w', 'a', 'l', 'c', 'a', 'v', 'w', 'v', 'r', 'w', 'o' });
+            b.Add(new[] { 'w', 'o', 'e', 't', 'l', 'o', 'l', 'm', 'k', 't', 'k', 'o', 'c', 'e' });
+            b.Add(new[] { 'o', 'e', 'w', 't', 'd', 'k', 't', 's', 'g', 'o', 'e', 'a', 'c', 'b' });
+            b.Add(new[] { 'f', 's', 'c', 'o', 'l', 'r', 'w', 't', 'c', 'm', 'o', 'a', 'm', 'e' });
+            b.Add(new[] { 'g', 'k', 'v', 'f', 'c', 'r', 'h', 'o', 'a', 'o', 'k', 'i', 't', 'g' });
+            b.Add(new[] { 'l', 'u', 'a', 'e', 'r', 'a', 'w', 'o', 't', 'o', 's', 'f', 'e', 't' });
+            b.Add(new[] { 'g', 'e', 'k', 'k', 't', 'r', 'w', 'e', 'w', 'f', 't', 'a', 'i', 'l' });
+            b.Add(new[] { 'l', 'o', 'e', 'i', 'o', 'o', 't', 'r', 'w', 'e', 'f', 'f', 't', 't' });
+            b.Add(new[] { 'o', 'e', 'r', 't', 'e', 'g', 'c', 't', 'i', 'o', 'a', 'c', 'l', 'w' });
+            b.Add(new[] { 'u', 'b', 'u', 't', 't', 'o', 'y', 'g', 's', 'r', 'o', 'k', 'r', 't' });
+            b.Add(new[] { 'l', 'r', 'f', 'y', 'h', 'a', 'a', 'a', 't', 'e', 'r', 'f', 'e', 't' });
+            
+            return b.ToArray();
+        }
+
+        private List<string> GetTestWords()
+        {
+            return new List<string>()
+            {
+                "kitty",
+                "ears",
+                "dog",
+                "fur",
+                "teeth",
+                "woof",
+                "bark",
+                "tail",
+                "meow",
+                "claw",
+                "vet",
+                "cat"
+            };
+        }
+    }
+}
---
+++ WordSearchSolver/Solver.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSearchSolver
+{
+    public class Solver
+    {
+        public List<string> WordList;
+        public char[][] Board;
+        public bool AllowSingleLetterWords;
+        public Solver()
+        {
+            WordList = File.ReadAllLines("words.txt").Select(x => x.ToUpper()).ToList();
+        }
+
+        public List<SearchResult> FindWords()
+        {
+            NormalizeBoard();
+            var foundWords = new List<SearchResult>();
+            var cx = 0;
+            var cy = 0;
+
+            for (int y = 0; y < Board.Length; y++)
+            {
+                var width = Board[y].Length;
+                for (int x = 0; x < width; x++)
+                {
+                    var currentWord = Board[y][x].ToString();
+                    var currentPositions = new List<Point> {new(x, y)};
+                    cy = y;
+                    cx = x;
+                    var letterStartWords = WordList.Where(w => w.StartsWith(currentWord));
+
+                    //Single Letter words
+                    var possibleWords = letterStartWords.Where(w => w.StartsWith(currentWord));
+                    if (AllowSingleLetterWords)
+                    if (possibleWords.Any(x => x.ToUpper() == currentWord.ToUpper()))
+                        foundWords.Add(new SearchResult
+                        {
+                            Word = currentWord,
+                            Positions = currentPositions
+                        });
+
+                    //left
+
+                    while (possibleWords.Any() && cx<width-1)
+                    {
+                        cx += 1;
+                        currentWord += Board[cy][cx].ToString();
+                        currentPositions.Add(new Point(cx, cy));
+                        possibleWords = possibleWords.Where(w => w.StartsWith(currentWord)).ToList();
+                        if (possibleWords.Any(x => x.ToUpper() == currentWord.ToUpper()))
+                            foundWords.Add(new SearchResult
+                            {
+                                Word = currentWord,
+                                Positions = currentPositions
+                            });
+
+                    }
+
+                    //Reset for right search
+
+                     currentWord = Board[y][x].ToString();
+                     currentPositions = new List<Point> { new(x, y) };
+                     cy = y;
+                     cx = x;
+                     possibleWords = letterStartWords.Where(w => w.StartsWith(currentWord));
+
+                    //right (reversed)
+
+                    while (possibleWords.Any() && cx > 0)
+                    {
+                        cx -= 1;
+                        currentWord += Board[cy][cx].ToString();
+                        currentPositions.Add(new Point(cx, cy));
+                        possibleWords = possibleWords.Where(w => w.StartsWith(currentWord)).ToList();
+
+                        if (possibleWords.Any(w => w == "dog"))
+                        {
+                            Console.WriteLine($"Possible dog: {x},{y} - {currentWord}");
+                        }
+
+                        if (possibleWords.Any(x => x.ToUpper() == currentWord.ToUpper()))
+                            foundWords.Add(new SearchResult
+                            {
+                                Word = currentWord,
+                                Positions = currentPositions
+                            });
+
+                    }
+
+
+                    //Reset for Up search
+
+                    currentWord = Board[y][x].ToString();
+                    currentPositions = new List<Point> { new(x, y) };
+                    cy = y;
+                    cx = x;
+                    possibleWords = letterStartWords.Where(w => w.StartsWith(currentWord));
+
+                    //Up
+                    while (possibleWords.Any() && cy > 0)
+                    {
+                        cy -= 1;
+                        currentWord += Board[cy][cx].ToString();
+                        currentPositions.Add(new Point(cx, cy));
+                        possibleWords = possibleWords.Where(w => w.StartsWith(currentWord)).ToList();
+                        if (possibleWords.Any(x => x.ToUpper() == currentWord.ToUpper()))
+                            foundWords.Add(new SearchResult
+                            {
+                                Word = currentWord,
+                                Positions = currentPositions
+                            });
+
+                    }
+
+
+                    //Reset for down search
+
+                    currentWord = Board[y][x].ToString();
+                    currentPositions = new List<Point> { new(x, y) };
+                    cy = y;
+                    cx = x;
+                    possibleWords = letterStartWords.Where(w => w.StartsWith(currentWord));
+
+                    //down
+                    while (possibleWords.Any() && cy < Board.Length-1)
+                    {
+                        cy += 1;
+                        currentWord += Board[cy][cx].ToString();
+                        currentPositions.Add(new Point(cx, cy));
+                        possibleWords = possibleWords.Where(w => w.StartsWith(currentWord)).ToList();
+                        if (possibleWords.Any(x => x.ToUpper() == currentWord.ToUpper()))
+                            foundWords.Add(new SearchResult
+                            {
+                                Word = currentWord,
+                                Positions = currentPositions
+                            });
+
+                    }
+
+
+                    //Reset for Diag Search
+
+                    currentWord = Board[y][x].ToString();
+                    currentPositions = new List<Point> { new(x, y) };
+                    cy = y;
+                    cx = x;
+                    possibleWords = letterStartWords.Where(w => w.StartsWith(currentWord));
+
+                    //Down-Right Diag
+                    while (possibleWords.Any() && cy < Board.Length - 1 && cx < width - 1)
+                    {
+                        cy += 1;
+                        cx += 1;
+                        currentWord += Board[cy][cx].ToString();
+                        currentPositions.Add(new Point(cx, cy));
+                        possibleWords = possibleWords.Where(w => w.StartsWith(currentWord)).ToList();
+                        if (possibleWords.Any(x => x.ToUpper() == currentWord.ToUpper()))
+                            foundWords.Add(new SearchResult
+                            {
+                                Word = currentWord,
+                                Positions = currentPositions
+                            });
+
+                    }
+
+                    //Reset for Diag Search
+
+                    currentWord = Board[y][x].ToString();
+                    currentPositions = new List<Point> { new(x, y) };
+                    cy = y;
+                    cx = x;
+                    possibleWords = letterStartWords.Where(w => w.StartsWith(currentWord));
+
+                    //Down-Left Diag
+                    while (possibleWords.Any() && cy < Board.Length - 1 && cx > 0)
+                    {
+                        cy += 1;
+                        cx -= 1;
+                        currentWord += Board[cy][cx].ToString();
+                        currentPositions.Add(new Point(cx, cy));
+                        possibleWords = possibleWords.Where(w => w.StartsWith(currentWord)).ToList();
+                        if (possibleWords.Any(x => x.ToUpper() == currentWord.ToUpper()))
+                            foundWords.Add(new SearchResult
+                            {
+                                Word = currentWord,
+                                Positions = currentPositions
+                            });
+
+                    }
+
+                    //Reset for Diag Search
+
+                    currentWord = Board[y][x].ToString();
+                    currentPositions = new List<Point> { new(x, y) };
+                    cy = y;
+                    cx = x;
+                    possibleWords = letterStartWords.Where(w => w.StartsWith(currentWord));
+
+                    //Up-Right Diag
+                    while (possibleWords.Any() && cy > 0 && cx < width - 1)
+                    {
+                        cy -= 1;
+                        cx += 1;
+                        currentWord += Board[cy][cx].ToString();
+                        currentPositions.Add(new Point(cx, cy));
+                        possibleWords = possibleWords.Where(w => w.StartsWith(currentWord)).ToList();
+                        if (possibleWords.Any(x => x.ToUpper() == currentWord.ToUpper()))
+                            foundWords.Add(new SearchResult
+                            {
+                                Word = currentWord,
+                                Positions = currentPositions
+                            });
+
+                    }
+
+                    //Reset for Diag Search
+
+                    currentWord = Board[y][x].ToString();
+                    currentPositions = new List<Point> { new(x, y) };
+                    cy = y;
+                    cx = x;
+                    possibleWords = letterStartWords.Where(w => w.StartsWith(currentWord));
+
+                    //Up-Left Diag
+                    while (possibleWords.Any() && cy > 0 && cx > 0)
+                    {
+                        cy -= 1;
+                        cx -= 1;
+                        currentWord += Board[cy][cx].ToString();
+                        currentPositions.Add(new Point(cx, cy));
+                        possibleWords = possibleWords.Where(w => w.StartsWith(currentWord)).ToList();
+                        if (possibleWords.Any(x => x.ToUpper() == currentWord.ToUpper()))
+                            foundWords.Add(new SearchResult
+                            {
+                                Word = currentWord,
+                                Positions = currentPositions
+                            });
+
+                    }
+
+
+                }
+
+            }
+
+            foundWords.ForEach(word => word.Positions = word.Positions.Take(word.Word.Length).ToList()); //make sure we didnt get any extra letters at the end
+            return foundWords;
+        }
+
+        private void NormalizeBoard()
+        {
+
+            for (int i = 0; i < Board.Length; i++)
+            {
+                for (int j = 0; j < Board[i].Length; j++)
+                {
+                    Board[i][j] = Board[i][j].ToString().ToUpper().ToCharArray()[0];
+                }
+            }
+        }
+    }
+
+    public class SearchResult
+    {
+        public string Word;
+        public List<Point> Positions;
+        public override string ToString()
+        {
+            return Word;
+        }
+    }
+}
